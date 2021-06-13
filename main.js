7116551290bfb99f79fd512fcaf7832e1b0263cd
ken kibet <ken.kibet@student.moringaschool.com>
2021-06-13 14:24:48 +0300

    added javascript file

---
+++ main.js
@@ -0,0 +1,6 @@
+document.onload=()=>{
+    var akanMaleName = [ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
+    var akanFemaleNames = [ "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua", "Ama"];
+
+
+}
